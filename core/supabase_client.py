ff1179b2ee746e19de8e5ffe5b38ff2f86203715
Kristof Doman <domankristof@gmail.com>
2025-11-24 15:48:00 +0100

    Updated .gitignore.

--- core/supabase_client.py
+++ core/supabase_client.py
@@ -2,6 +2,7 @@ from supabase import create_client
 import os
 
 SUPABASE_URL = "https://nlpoglsfvykbkepfhast.supabase.co"
+SUPABASE_KEY = "SUPABASE_KEY"   # Not anon key
 BUCKET = "telemetry"
 
-supabase = create_client(SUPABASE_URL)
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
